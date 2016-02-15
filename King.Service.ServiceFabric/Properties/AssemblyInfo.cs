fd1e4763e0e3878fe6c3e8fc273c2e2f9612d2a8
Jef King <jefkin@microsoft.com>
2016-02-14 16:08:18 -0800

    build out

---
+++ King.Service.ServiceFabric/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Service.ServiceFabric")]
+[assembly: AssemblyDescription("Task scheduling for Azure and Windows: Service Fabric.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Service.ServiceFabric")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e8fb7b7e-b404-4bf3-981f-2d7398d22059")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
