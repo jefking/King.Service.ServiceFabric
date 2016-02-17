e93fac5e2da53fcac25d4f79b47ced2c5d2edbfe
Jef King <jefkin@microsoft.com>
2016-02-16 18:05:57 -0800

    ,more goodness

---
+++ King.Service.ServiceFabric/Queue.cs
@@ -0,0 +1,22 @@
+﻿namespace King.Service.ServiceFabric
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Data.Collections;
+    using Microsoft.ServiceFabric.Services.Communication.Runtime;
+    using Microsoft.ServiceFabric.Services.Runtime;
+    using Microsoft.ServiceFabric.Data;
+
+    public class Queue
+    {
+        protected readonly IReliableStateManager manager = null;
+
+        public Queue(IReliableStateManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
