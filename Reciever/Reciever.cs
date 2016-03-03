a443b544cea4ba68644adf2ec2dc24f7d7c17046
Jef King <jef@agilebusinesscloud.com>
2016-03-02 22:49:52 -0800

    Misc

--- Reciever/Reciever.cs
+++ Reciever/Reciever.cs
@@ -1,14 +1,15 @@
 ﻿namespace Reciever
 {
+    using King.Service;
     using King.Service.ServiceFabric;
 
     /// <summary>
     /// The FabricRuntime creates an instance of this class for each service type instance.
     /// </summary>
-    internal sealed class Reciever : DequeueService<string>
+    internal sealed class Reciever : RoleService<object>
     {
         public Reciever()
-            : base("KingQueue", new DataProcessor())
+            : base(new RoleTaskManager<object>())
         {
         }
     }
