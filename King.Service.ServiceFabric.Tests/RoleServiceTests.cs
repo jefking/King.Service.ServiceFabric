84aa86465a132373792d4c9aefc3c27b50b3af72
Jef King <jef@agilebusinesscloud.com>
2016-03-02 22:30:00 -0800

    updates

---
+++ King.Service.ServiceFabric.Tests/RoleServiceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace King.Service.ServiceFabric.Tests
+{
+    using System;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RoleServiceTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var manager = Substitute.For<IRoleTaskManager<object>>();
+            new RoleService<object>(manager);
+        }
+
+        [Test]
+        public void ConstructorWithConfig()
+        {
+            var manager = Substitute.For<IRoleTaskManager<object>>();
+            new RoleService<object>(manager, new object());
+        }
+
+        [Test]
+        public void ConstructorhNullConfig()
+        {
+            var manager = Substitute.For<IRoleTaskManager<object>>();
+            new RoleService<object>(manager, null);
+        }
+
+        [Test]
+        public void ConstructorManagerNull()
+        {
+            Assert.That(() => new RoleService<object>(null), Throws.TypeOf<ArgumentNullException>());
+        }
+    }
+}
---
+++ King.Service.ServiceFabric/RoleService.cs
@@ -0,0 +1,71 @@
+﻿namespace King.Service.ServiceFabric
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Services.Runtime;
+
+    /// <summary>
+    /// Role Service
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RoleService<T> : StatelessService
+    {
+        #region Members
+        /// <summary>
+        /// Configuration
+        /// </summary>
+        protected readonly T configuration;
+
+        /// <summary>
+        /// Task Manager
+        /// </summary>
+        protected readonly IRoleTaskManager<T> taskManager;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Role Service Constructor
+        /// </summary>
+        /// <param name="taskManager">Task Manager</param>
+        /// <param name="config">Configuration</param>
+        public RoleService(IRoleTaskManager<T> taskManager, T config = default(T))
+        {
+            if (null == taskManager)
+            {
+                throw new ArgumentNullException("taskManager");
+            }
+
+            this.taskManager = taskManager;
+            this.configuration = config;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Run Async
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>Task</returns>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            if (this.taskManager.OnStart(this.configuration))
+            {
+                this.taskManager.Run();
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                }
+
+                this.taskManager.OnStop();
+            }
+            else
+            {
+                Trace.TraceError("Task Manager failed to start. Service not running.");
+            }
+        }
+        #endregion
+    }
+}
