1c4a3f0d75185ea2a3a8086ff4c27c9325aeb79d
Jef King <jef@agilebusinesscloud.com>
2016-03-05 08:33:16 -0800

    misc

--- King.Service.ServiceFabric.Tests/Properties/AssemblyInfo.cs
+++ King.Service.ServiceFabric.Tests/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("King.Service.ServiceFabric.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit test cases for task scheduling for Service Fabric.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("King.Service.ServiceFabric.Tests")]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d63e1201-dcb0-46fa-b872-9df4734055e3")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
--- King.Service.ServiceFabric/RoleService.cs
+++ King.Service.ServiceFabric/RoleService.cs
@@ -1,10 +1,10 @@
 ﻿namespace King.Service.ServiceFabric
 {
-    using Microsoft.ServiceFabric.Services.Runtime;
     using System;
     using System.Diagnostics;
     using System.Threading;
     using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Services.Runtime;
 
     /// <summary>
     /// Role Service
