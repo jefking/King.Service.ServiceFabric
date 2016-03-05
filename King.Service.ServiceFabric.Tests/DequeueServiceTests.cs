21830b390a52ffb0721eda6e39221065845faf94
Jef King <jef@agilebusinesscloud.com>
2016-03-05 08:38:30 -0800

    Constructor Tests

---
+++ King.Service.ServiceFabric.Tests/DequeueServiceTests.cs
@@ -0,0 +1,31 @@
+﻿namespace King.Service.ServiceFabric.Tests
+{
+    using System;
+    using Azure.Data;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DequeueServiceTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var processor = Substitute.For<IProcessor<object>>();
+            new DequeueService<object>(Guid.NewGuid().ToString(), processor);
+        }
+
+        [Test]
+        public void ConstructorQueueNameNull()
+        {
+            var processor = Substitute.For<IProcessor<object>>();
+            Assert.That(() => new DequeueService<object>(null, processor), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void ConstructorProcessorNull()
+        {
+            Assert.That(() => new DequeueService<object>(Guid.NewGuid().ToString(), null), Throws.TypeOf<ArgumentNullException>());
+        }
+    }
+}
