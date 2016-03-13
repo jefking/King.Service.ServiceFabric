f46b11d450366c74defeba2561939b32bb54ef23
Jef King <jef@agilebusinesscloud.com>
2016-03-12 20:10:15 -0800

    Task service, simple single task.

---
+++ King.Service.ServiceFabric/TaskService.cs
@@ -0,0 +1,64 @@
+﻿namespace King.Service.ServiceFabric
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Services.Runtime;
+
+    /// <summary>
+    /// Task Service
+    /// </summary>
+    /// <typeparam name="T">Task</typeparam>
+    public class TaskService : StatelessService
+    {
+        #region Members
+        /// <summary>
+        /// Runnable Task
+        /// </summary>
+        protected readonly IRunnable run;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Role Service Constructor
+        /// </summary>
+        /// <param name="run">Task Manager</param>
+        public TaskService(IRunnable run)
+        {
+            if (null == run)
+            {
+                throw new ArgumentNullException("run");
+            }
+
+            this.run = run;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Run Async
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>Task</returns>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            this.run.Start();
+
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                Trace.TraceError("Task Canceled Exception, can be normal: {0}", ex);
+            }
+
+            this.run.Stop();
+        }
+        #endregion
+    }
+}
